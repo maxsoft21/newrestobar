6c3bd80bf6b294f816b4b2b3048524510b9dd89e
Max Samir Gabriel Leon Quiroz <mleonqui@emeal.nttdata.com>
2023-09-16 18:26:19 -0500

    add node

---
+++ server.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    const filePath = path.join(__dirname, 'index.html');
+    const stream = fs.createReadStream(filePath);
+
+    res.writeHead(200, {
+        'Content-Type': 'text/html',
+        'Cache-Control': 'no-cache'
+    });
+
+    stream.pipe(res);
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Servidor en ejecución en http://localhost:${PORT}`);
+});
